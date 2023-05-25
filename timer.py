6c55ac0f795d42a9f5422b9fa632bde1fb9f7cd6
Martin864 <potancokmartas@seznam.cz>
2023-05-25 15:17:31 +0200

    Create timer.py

---
+++ timer.py
@@ -0,0 +1,27 @@
+from tkinter import *
+import time
+
+class timerxd:
+    def __init__(self,master):
+        self.master = master
+        self.timer_label = Label(self.master, text = "00:00")
+        self.timer_label.pack()
+        self.start_time = time.time()
+        self.update_timer()
+
+    def update_timer(self):
+        elapsed_time = time.time() - self.start_time
+        minutes = int(elapsed_time // 60)
+        seconds = int(elapsed_time % 60)
+        timer_string = "{:2d}:{:2d}".format(minutes, seconds)
+        self.timer_label.config(text = timer_string)
+        self.master.after(1000, self.update_timer)
+        
+root = Tk()
+
+timer_app = timerxd(root)
+
+mainloop()
+        
+        
+        
