0472d8e0b169e4f575fb2581774f956e94d928b2
Martin864 <potancokmartas@seznam.cz>
2023-05-25 16:04:31 +0200

    Update timer.py

--- timer.py
+++ timer.py
@@ -4,7 +4,7 @@ import time
 class timerxd:
     def __init__(self,master):
         self.master = master
-        self.timer_label = Label(self.master, text = "00:00")
+        self.timer_label = Label(self.master, text = "00:00", fg = "blue", font = ("Arial", 30), bg = "gray", padx = 20, pady = 10)
         self.timer_label.pack()
         self.start_time = time.time()
         self.update_timer()
@@ -13,7 +13,7 @@ class timerxd:
         elapsed_time = time.time() - self.start_time
         minutes = int(elapsed_time // 60)
         seconds = int(elapsed_time % 60)
-        timer_string = "{:2d}:{:2d}".format(minutes, seconds)
+        timer_string = "{:1d}:{:1d}".format(minutes, seconds)
         self.timer_label.config(text = timer_string)
         self.master.after(1000, self.update_timer)
         
@@ -21,7 +21,6 @@ root = Tk()
 
 timer_app = timerxd(root)
 
-mainloop()
-        
+mainloop()        
         
         
