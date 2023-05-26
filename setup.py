fadbab5faa0f061b6345656f02edc7dbad28cef6
fildaletnany <filip.zrost@gmail.com>
2023-05-26 09:10:12 -0700

    ds

--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
 from tkinter import *
 import random
 
+mines_coordinates = NONE
+row_count = 0
+column_count = 0
+mine_count = 0
+result = NONE
+
+def check_for_duplicates ():
+    for x in mines_coordinates:
+        for y in mines_coordinates:
+            if(x[0] == y[0] and x[1] == y[1]):
+                x[0] = random.randint(0,row_count)
+                x[1] = random.randint(0,column_count) 
+
+
 def zobraz_vstup():
     global e1
     global e2
@@ -11,27 +25,53 @@ def zobraz_vstup():
     mine_count = int(e3.get())
 
     mines_coordinates = []
-    global already_generated
-    global result
-    result  = 'from tkinter import *'
-    result  += 'import os, sys'
-    result  += 'root = Tk()'
-    result  += 'mine_image = PhotoImage(file=r"" + os.getcwd() + "\mine.png")'
-    result  += 'one_image = PhotoImage(file=r"" + os.getcwd() + "\_one.png")'
-    result  += 'two_image = PhotoImage(file=r"" + os.getcwd() + "\_two.png")'
-    result  += 'three_image = PhotoImage(file=r"" + os.getcwd() + "\_three.png")'
-    result  += 'gridFrame = Frame(root).grid(row=0,column=0)'
+    result  = 'from tkinter import *\n'
+    result  += 'import os, sys\n'
+    result  += 'root = Tk()\n'
+    result  += 'mine_image = PhotoImage(file=r"" + os.getcwd() + "\mine.png")\n'
+    result  += 'one_image = PhotoImage(file=r"" + os.getcwd() + "\_one.png")\n'
+    result  += 'two_image = PhotoImage(file=r"" + os.getcwd() + "\_two.png")\n'
+    result  += 'three_image = PhotoImage(file=r"" + os.getcwd() + "\_three.png")\n'
+    result  += 'gridFrame = Frame(root).grid(row=0,column=0)\n'
     
     #dvourozmerny array vytvorenych min
     index = 0
     while(index <= mine_count):
         row_generated = random.randint(0,row_count)
         column_generated = random.randint(0,column_count)
+        #nova prazdna mina
         mines_coordinates.append([])
+        print(row_generated)
+        print(column_generated)
         print(index)
+        #vsechny miny 
         print(mines_coordinates)
         
-        for r in mines_coordinates:
+        mines_coordinates[index].append(row_generated)
+        mines_coordinates[index].append(column_generated)
+        mines_coordinates[index].append(index)
+        index  += 1
+
+        
+        
+        """for r in mines_coordinates:
+            if(len(r) != 0):
+                if(r[0] == row_generated and r[1] == column_generated):
+                    print("same detected")
+                else:                 
+                    mines_coordinates[index].append(row_generated)
+                    mines_coordinates[index].append(column_generated)
+                    index  += 1
+                    print("mine added inner")
+            else:
+                mines_coordinates[index].append(row_generated)
+                mines_coordinates[index].append(column_generated)
+                index  += 1
+                print("mine added outer")
+        print("done")"""
+
+        
+        """for r in mines_coordinates:
             if(len(r) >= 2):
                 if(r[0] == row_generated and r[1] == column_generated):
                     already_generated = True
@@ -39,23 +79,42 @@ def zobraz_vstup():
                     mines_coordinates[index].append(row_generated)
                     mines_coordinates[index].append(column_generated)
                     index  += 1
-                    print("success, current mines_coordinates: " + mines_coordinates + " -- current index: " + index)
+                    print("BBBBBBBBBBBBBBBBBBBBBB" + str(index))
+                    #print("success, current mines_coordinates: " + mines_coordinates + " -- current index: " + index)
             else:
                 mines_coordinates[index].append(row_generated)
                 mines_coordinates[index].append(column_generated)
                 index  += 1
-                print("success, current mines_coordinates: " + mines_coordinates + " -- current index: " + index)
-    for x in mines_coordinates:
-        
-        result += 'Button(gridFrame, width = "40",height = "40",command="mine").grid(row=' + str(x[0]) + ',column=' + str(x[1]) + ')'
+                print("AAAAAAAAAAAAAAAAAA" + str(index))
+                #print("success, current mines_coordinates: " + mines_coordinates + " -- current index: " + index)
+    """
 
+    for x in mines_coordinates:
+        result += 'Button(gridFrame, width = "40",height = "40",command="mine").grid(row=' + str(x[0]) + ',column=' + str(x[1]) + ')\n'
+    
+    index_row = 0
+    index_column = 0
+    coords_taken_bool = False
+    while(index_row <= row_count):
+        while (index_column <= column_count):
+            for a in mines_coordinates:
+                if(index_row == a[0] and index_column == a[1]):
+                    coords_taken_bool = True
+            if(coords_taken_bool == False):
+                result += 'Button(gridFrame, width = "40",height = "40",command="mine").grid(row=' + str(index_row) + ',column=' + str(index_column) + ')\n'
+            else:
+                print("coordinates already taken, skipping")
+
+            index_column += 1
+            coords_taken_bool = False
+        index_row += 1
+        #tam kde neni mina vygeneruj normalni policko
+
+    result += "root.mainloop()"
     print(result)
-    #dva generatory cisel, jeden pro column, druhy pro  row
-    #check jestli uz takovy souradnice nejsou zabrany 
-    #Button(gridFrame, width = "40",height = "40",command="mine").grid(row=row_generated,column=column_generated)
     
-    
-    #main = open("main.py","x")
+    main = open("main.py","w")
+    main.write(result)
     
     #root.destroy()
 
